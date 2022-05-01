05efef3d850c9149c2bf427bf0f26d425f2502b8
Kortney <kortney@iMac.local>
2022-05-01 19:08:00 -0300

    added fetch to our search function, it fetches from the API the query that we pass in (the one we get from our form). Made a new function that updates the page with the data from the search results.

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ function search(e) {
     const form = new FormData(this);
     const query = form.get("search");
     console.log(query);
+
+    fetch(`https://api.jikan.moe/v4/anime?q=${query}`)
+        .then((response) => response.json())
+        .then(updatePage);
 }
+
+function updatePage(jsonResponse) {
+    var searchResults = jsonResponse.data;
+    var results = document.getElementById("results");
+    var resultsInnerHTML = `
+    <h1>${searchResults[0].url}</h1>
+    `;
+    results.innerHTML = resultsInnerHTML;
+}
+
 function pageLoaded() {
     const form = document.getElementById("searchForm");
     form.addEventListener("submit", search);
