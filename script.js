b677fd8a1d51cee2503e31bc6e3417d02b776afb
Kortney <kortney@iMac.local>
2022-05-11 20:40:01 -0300

    added dropdown selector for status (airing, complete, upcoming). Added selected values in the dropdown field that allow you to not select a category and tell you what the dropdown contains i.e: rating

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function search(formSubmitEvent) {
     formSubmitEvent.preventDefault();
-    const formData = new FormData(this); //this means whatever the function is getting, refers to an object
+    const formData = new FormData(this); //populates our new formdata obj with all the data from form
     const query = formData.get("search");
     const rating = formData.get("rating");
     const animeType = formData.get("animeType");
+    const status = formData.get("status");
     console.log(query);
 
     fetch(
-        `https://api.jikan.moe/v4/anime?q=${query}&rating=${rating}&type=${animeType}`
+        `https://api.jikan.moe/v4/anime?q=${query}&rating=${rating}&type=${animeType}&status=${status}`
     )
         .then((response) => response.json())
         .then(updatePage);
